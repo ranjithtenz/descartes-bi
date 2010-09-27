af614d3601a8847a4f1e07a09a677700091c4981
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2010-09-26 21:53:44 -0400

    Added new option to enable or disable serving of static content

--- apps/tornado_app/management/commands/runtornado.py
+++ apps/tornado_app/management/commands/runtornado.py
@@ -4,17 +4,24 @@ import os
 import sys
  
 class Command(BaseCommand):
-    option_list = BaseCommand.option_list + ()
+    option_list = BaseCommand.option_list + (
+    make_option('--dontservestatic',
+        action='store_false',
+        dest='serve_static',
+        default=True,
+        help='Do not serve static content.  Otherwise static content will be served on http://<same ip>:<port + 1>/static/'),    
+    )
     help = "Starts a Tornado Web."
     args = '[optional port number, or ipaddr:port]'
+
+    sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
+    sys.stderr = os.fdopen(sys.stderr.fileno(), 'w', 0)
  
     def handle(self, addrport='', *args, **options):
-        import django
         from django.core.handlers.wsgi import WSGIHandler
         from tornado import httpserver, wsgi, ioloop, web
- 
-	sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
-	sys.stderr = os.fdopen(sys.stderr.fileno(), 'w', 0)
+
+        import django
  
         if args:
             raise CommandError('Usage: runtornado %s' % self.args)
@@ -34,26 +41,8 @@ class Command(BaseCommand):
  
         quit_command = (sys.platform == 'win32') and 'CTRL-BREAK' or 'CONTROL-C'
  
-        def inner_run():
+        def inner_run(serve_static = options['serve_static']):
             from django.conf import settings
-            
-#            class StaticHandler(web.RequestHandler):
-#                def get(self):
-#                    self.write("Hello, world")
-
-            #static_settings = {
-            #    "static_path":settings.MEDIA_ROOT,
-            #    "cookie_secret": "61oETzKXQAGaYdkL5gEmGeJJFuYh7EQnp2XdTP1o/Vo=",
-            #    "login_url": "/login",
-            #    "xsrf_cookies": True,
-            #}
-            #application = tornado.web.Application([
-            #    (r"/", MainHandler),
-            #    (r"/login", LoginHandler),
-            #], **settings)
-
-            #http_server.static_path=settings.MEDIA_ROOT
-                        
             
             print "Validating models..."
             self.validate(display_num_errors=True)
@@ -65,7 +54,7 @@ class Command(BaseCommand):
             django_server = httpserver.HTTPServer(django_container)
             django_server.listen(int(port), address=addr)
 
-            if 1:
+            if serve_static:
                 static_address=addr
                 static_port=int(port)+1
                 settings.SERVE_STATIC_CONTENT=False
